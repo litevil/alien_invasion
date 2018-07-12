b56979cf588eea4f481e20288c26df9a1a5a96b6
your name <your email>
2018-07-12 10:34:50 +0800

    1

---
+++ alien_invasion.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env/ python
+
+import sys
+import pygame
+from pygame.locals import *
+
+
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+    # 设置背景色
+    bg_color = (230, 230, 230)
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        pygame.display.flip()
+
+
+run_game()
+
+
+
+
